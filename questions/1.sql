54c2ef3dc3c3ecb51598437ebed960dd2ae4977c
180107266 <55758989+180107266@users.noreply.github.com>
2021-03-13 23:32:01 +0600

    Create 1.sql

---
+++ questions/1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE medical_center
+	(mcenter_id NUMBER(4), mcenter_name VARCHAR2(100 CHAR), city VARCHAR2(50 CHAR), 
+	address VARCHAR2(100 CHAR), contacts NUMBER(11), 
+	CONSTRAINT medical_center_pk PRIMARY KEY (mcenter_id) );
+
+/*How the table of medical centers was created?*/
